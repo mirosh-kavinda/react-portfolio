c60298399080473f89c5386c234fd3544ab384b4
mirosh_kavinda <kavindahgm@gmail.com>
2022-07-13 17:49:51 +0530

    commit secon commit

---
+++ build/precache-manifest.3ce6489d6a8c87dfd95bc6f6cda7be0d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "54f5d6be0bc80c396038",
+    "url": "/react-portfolio/static/css/main.d37a63d1.chunk.css"
+  },
+  {
+    "revision": "54f5d6be0bc80c396038",
+    "url": "/react-portfolio/static/js/main.54f5d6be.chunk.js"
+  },
+  {
+    "revision": "018936f8e499cd4e1e86",
+    "url": "/react-portfolio/static/css/1.329eed26.chunk.css"
+  },
+  {
+    "revision": "018936f8e499cd4e1e86",
+    "url": "/react-portfolio/static/js/1.018936f8.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "dc2f59daff220ad23f73dfd1c7f97a12",
+    "url": "/react-portfolio/static/media/home-image2.dc2f59da.png"
+  },
+  {
+    "revision": "c718b7d0abe16bc9d0e7d0d9cc7645b3",
+    "url": "/react-portfolio/static/media/home-proj1.c718b7d0.png"
+  },
+  {
+    "revision": "3b0b272ba82852d863efd995ebeeeedd",
+    "url": "/react-portfolio/static/media/home-proj2.3b0b272b.png"
+  },
+  {
+    "revision": "7100dd97e8b4d03a5e72c0e01378f3ec",
+    "url": "/react-portfolio/static/media/home-image1.7100dd97.png"
+  },
+  {
+    "revision": "dc2f59daff220ad23f73dfd1c7f97a12",
+    "url": "/react-portfolio/static/media/about-image1.dc2f59da.png"
+  },
+  {
+    "revision": "0969b7a26882684a1f5f24fa27f4ee0c",
+    "url": "/react-portfolio/static/media/about-image2.0969b7a2.png"
+  },
+  {
+    "revision": "2c93681337768fcfce3d3f7ee63bda2a",
+    "url": "/react-portfolio/static/media/about-image3.2c936813.png"
+  },
+  {
+    "revision": "7c4ae945b4dcba1078bc2537e7e7b4a6",
+    "url": "/react-portfolio/static/media/contact-image1.7c4ae945.png"
+  },
+  {
+    "revision": "eedd90edb78624ccbdc6f7ee4547eb26",
+    "url": "/react-portfolio/static/media/project-image1.eedd90ed.png"
+  },
+  {
+    "revision": "37174b3e421c941c14728ab54161ed5b",
+    "url": "/react-portfolio/static/media/project-image2.37174b3e.png"
+  },
+  {
+    "revision": "2145943cb02a7bd0c4b1ba57e706066f",
+    "url": "/react-portfolio/index.html"
+  }
+];
